fbdf1dd0cab339ed0016a61ec15c4919b18b73ab
Rishabh-Sh1rma <130218510+Rishabh-Sh1rma@users.noreply.github.com>
2023-04-23 00:59:32 +0530

    Add files via upload

---
+++ BUSSINESS1/connect.php
@@ -0,0 +1,12 @@
+<?php
+
+// Create connection
+$con =mysqli_connect('localhost', 'root', '','site');
+if(isset($_POST['sb']))
+{
+  $email = $_POST['email'];
+  $sql = "INSERT INTO  register(email) VALUE('$email');";
+  $run = mysqli_query($con, $sql);
+}
+?>
+ 
